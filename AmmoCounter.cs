6b5b486dda2e75686c5d7e36222122292c3e3a73
kiobu <cdolson30@gmail.com>
2021-09-26 19:55:43 -0400

    initial

---
+++ AmmoCounter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using Comfort.Common;
+using EFT;
+using UnityEngine;
+
+namespace Kiobu.AmmoDisplay
+{
+    public class AmmoCounter : MonoBehaviour
+    {
+        void OnGUI()
+        {
+            if (InWorld())
+            {
+                Player player = LocalPlayer();
+                var ammo = player.Weapon.GetCurrentMagazine()?.Cartridges.Count;
+                var chamber = player.Weapon.ChamberAmmoCount;
+
+                var magmax = player.Weapon.GetCurrentMagazine()?.Cartridges.MaxCount;
+                
+                if (ammo == null)
+                {
+                    return;
+                }
+                else
+                {
+                    ammo += chamber;
+                    var ammoString = ((int)ammo).ToString();
+                    GUI.Label(new Rect(Screen.width - 400, Screen.height - 200, 400, 200), "Ammo: " + ammoString + " / " + magmax);
+                }
+            }
+        }
+
+        private static bool InWorld()
+        {
+            return Singleton<GameWorld>.Instance != null;
+        }
+
+        private static Player LocalPlayer()
+        {
+            if (InWorld())
+            {
+                return Singleton<GameWorld>.Instance.RegisteredPlayers.Find(p => p.IsYourPlayer);
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Kiobu.AmmoDisplay
+{
+    internal class Program
+    {
+        private static GameObject hookObject;
+        public static void Main(string[] args)
+        {
+            Debug.LogError("Loading Kiobu's ammo display mod.");
+            hookObject = new GameObject();
+            hookObject.AddComponent<AmmoCounter>();
+            Object.DontDestroyOnLoad(hookObject);
+        }
+    }
+}
